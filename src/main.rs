a457550cb0e19088d2213ded47fb5c198caa2edb
Fahri Cihan Demirci <femnad@users.noreply.github.com>
2019-07-18 22:13:27 +0100

    fisher price my first subcommand
    that's a joke which never gets old amirite?

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,11 @@ extern crate tantivy;
 
 use std::path::Path;
 use std::process::Command;
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand, AppSettings};
 use reqwest::StatusCode;
 use roxmltree::Document;
 use tantivy::schema::Schema;
+use tantivy::schema::Field;
 use tantivy::{Index, Score, DocAddress};
 use tantivy::query::QueryParser;
 use tantivy::collector::TopDocs;
@@ -72,18 +73,25 @@ fn get_index(schema_path: &Path, schema: Schema) -> Index {
     if maybe_index.is_ok() {
         maybe_index.unwrap()
     } else {
+        if !schema_path.exists() {
+            std::fs::create_dir(schema_path).unwrap();
+        }
         Index::create_in_dir(schema_path, schema).unwrap()
     }
 }
 
-fn scrape_posts(posts: Vec<Post>, schema_dir: String) {
+fn get_schema() -> (Field, Field, Schema) {
     let mut schema_builder = Schema::builder();
     let title = schema_builder.add_text_field("title", tantivy::schema::TEXT | tantivy::schema::STORED);
     let body = schema_builder.add_text_field("body", tantivy::schema::TEXT);
-    let schema = schema_builder.build();
+    (title, body, schema_builder.build())
+}
+
+fn scrape_posts(posts: Vec<Post>, schema_dir: String) {
+    let (title, body, schema) = get_schema();
 
     let schema_path = Path::new(&schema_dir);
-    let index = get_index(schema_path, schema.clone());
+    let index = get_index(schema_path, schema);
 
     let mut index_writer = index.writer(100_000_000).expect("writer create fail");
 
@@ -107,48 +115,80 @@ fn scrape_posts(posts: Vec<Post>, schema_dir: String) {
     }
     index_writer.commit().expect("commit fail");
 
+}
+
+fn search(term: &str, results: usize, schema_path: &str) {
+    let (title, body, schema) = get_schema();
+    let index = Index::open_in_dir(schema_path).expect("index open fail");
     let reader = index.reader().expect("reader fail");
     let searcher = reader.searcher();
 
     let query_parser = QueryParser::for_index(&index, vec![title, body]);
 
-    let query = query_parser.parse_query("rust").expect("query parse fail");
+    let query = query_parser.parse_query(term).expect("query parse fail");
 
-    let top_docs: Vec<(Score, DocAddress)> = searcher.search(&query, &TopDocs::with_limit(10))
+    let top_docs: Vec<(Score, DocAddress)> = searcher.search(&query, &TopDocs::with_limit(results))
         .expect("search fail");
 
-    for (_score, doc_address) in top_docs {
+    for (score, doc_address) in top_docs {
         let retrieved_doc = searcher.doc(doc_address).expect("retrieve fail");
-        println!("{}", schema.to_json(&retrieved_doc));
+        println!("{score}: {doc}", score=score, doc=schema.to_json(&retrieved_doc));
     }
 }
 
 fn main() {
     let matches = App::new("gel")
-        .arg(Arg::with_name("secret")
-             .short("s")
-             .long("secret")
-             .help("a pass secret containing API secret")
-             .takes_value(true)
-             .required(true))
-        .arg(Arg::with_name("count")
-            .short("c")
-            .long("count")
-            .help("number of recent posts")
-            .default_value("1"))
-        .arg(Arg::with_name("index")
-             .short("i")
-             .long("index-path")
-             .takes_value(true)
-             .default_value(DEFAULT_INDEX_PATH))
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(SubCommand::with_name("index")
+            .about("build an index")
+            .arg(Arg::with_name("secret")
+                .short("s")
+                .long("secret")
+                .help("a pass secret containing API secret")
+                .takes_value(true)
+                .required(true))
+            .arg(Arg::with_name("index")
+                .short("i")
+                .long("index-path")
+                .takes_value(true)
+                .default_value(DEFAULT_INDEX_PATH))
+            .arg(Arg::with_name("count")
+                .short("c")
+                .long("count")
+                .help("number of recent posts")
+                .default_value("1")))
+        .subcommand(SubCommand::with_name("search")
+            .about("search the index")
+            .arg(Arg::with_name("term")
+                .index(1)
+                .required(true))
+            .arg(Arg::with_name("index")
+                .short("i")
+                .long("index-path")
+                .takes_value(true)
+                .default_value(DEFAULT_INDEX_PATH))
+            .arg(Arg::with_name("results")
+                .short("r")
+                .long("results")
+                .help("number of results")
+                .default_value("10")))
         .get_matches();
-    let pass_secret = matches.value_of("secret").expect("failed getting secret");
-    let count: u64 = matches.value_of("count").expect("failed getting count").parse()
-        .expect("failed parsing int");
-    let index_path = matches.value_of("index").unwrap();
 
-    let options = Options{secret: pass_secret.to_string(), count: count, index_path: index_path.to_string()};
-    let index_path = options.index_path.clone();
-    let posts = get_posts(options);
-    scrape_posts(posts, index_path)
+    if let Some(matches) = matches.subcommand_matches("index") {
+        let pass_secret = matches.value_of("secret").expect("failed getting secret");
+        let count: u64 = matches.value_of("count").expect("failed getting count").parse()
+            .expect("failed parsing int");
+        let index_path = matches.value_of("index").unwrap();
+
+        let options = Options{secret: pass_secret.to_string(), count,
+            index_path: index_path.to_string()};
+        let index_path = options.index_path.clone();
+        let posts = get_posts(options);
+        scrape_posts(posts, index_path)
+    } else if let Some(matches) = matches.subcommand_matches("search") {
+        let index_path = matches.value_of("index").unwrap();
+        let term = matches.value_of("term").unwrap();
+        let results: usize = matches.value_of("results").unwrap().parse().unwrap();
+        search(term, results, index_path);
+    }
 }
